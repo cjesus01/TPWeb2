190660c1028d1cef405836059bc62fa971e3d2bc
jesus <jesusm.cas96@gmail.com>
2022-10-05 17:59:28 -0300

    arreglo de error al bajar otra rama

--- app/views/ClothingView.php
+++ app/views/ClothingView.php
@@ -3,11 +3,12 @@ class ClothingView {
     public function ShowClothes($Clothing){
         echo '<ul>';
         foreach ($Clothing as $Clothes){
-            echo '<li>'.$Clothes->sexo.'</li>';
-            echo '<li>'.$Clothes->talla.'</li>';
-            echo '<li>'.$Clothes->color.'</li>';
-            echo '<li>'.$Clothes->prenda.'</li>';
-            echo '<li>'.$Clothes->tipo_de_tela.'</li>';
+            echo '<li>'.$Clothes->Sexo.'</li>';
+            echo '<li>'.$Clothes->Talla.'</li>';
+            echo '<li>'.$Clothes->Color.'</li>';
+            echo '<li>'.$Clothes->Tipo_de_prenda.'</li>';
+            echo '<li>'.$Clothes->Tipo_de_tela.'</li>';
+            echo '<li>'.$Clothes->Descripcion.'</li>';
         }
         echo '</ul>';
     }
